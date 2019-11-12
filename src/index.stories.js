1374b19187c48e2b08bf6674435a62c3497a4f62
GuilleLink <32148569+GuilleLink@users.noreply.github.com>
2019-11-12 15:58:48 -0600

    Instalacion de Yarn y algunas pruebas para el addon-knobs

--- src/index.stories.js
+++ src/index.stories.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Button } from '@storybook/react/demo';
 
+//Basado en este Codepen:
+//https://codepen.io/alvaromontoro/pen/BaaBYyz
+
 export default { title: 'Button' };
+
+
+
 
 export const withText = () => <Button>Hello Button</Button>;
 
 export const withEmoji = () => (
   <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
 );
+
+export const withTexEmoji = () => <Button> Hello World </Button>;
+
+
