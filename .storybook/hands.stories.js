728f8c80c808296471ac25eeeaed296ab94359ea
GuilleLink <32148569+GuilleLink@users.noreply.github.com>
2019-11-14 17:57:30 -0600

    Storyboards de las manos de los personajes

--- .storybook/hands.stories.js
+++ .storybook/hands.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from '@storybook/react/demo';
 import { action } from '@storybook/addon-actions';
 import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
+import style from 'react-syntax-highlighter/dist/styles/hljs/atelier-savanna-dark';
 
 //Basado en este Codepen:
 //https://codepen.io/alvaromontoro/pen/BaaBYyz
@@ -11,10 +12,16 @@ export default {
   decorators: [withKnobs]
 };
 
-export const YourHand = () =>(
-  <Button onClick={(action(''))}></Button>
-);
+export const YourHand = () => {
+  const name = text("Play", "Rock");
+  const content = `You picked ${name}.`;
 
-export const RivalHand = () =>(
-    <Button onClick={(action(''))}></Button>
-  );
+  return <div color="white">{content}</div>;
+};
+
+export const RivalHand = () => {
+  const name = text("Play", "Paper");
+  const content = `Computer picked ${name}.`;
+
+  return <div color="white">{content}</div>;
+};
